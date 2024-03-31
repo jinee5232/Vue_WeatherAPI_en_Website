53891eaeae5100e2dd3e388ea3af96df2eeb95c6
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-31 13:40:25 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Vue_WeatherAPI_en_Website/",
+  plugins: [vue()],
+});
